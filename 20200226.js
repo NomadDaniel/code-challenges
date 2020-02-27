33b134873ea1e675a4dc146deba7e9f8d0ea50d9
NomadDaniel <reachingdaniel@gmail.com>
2020-02-27 00:37:54 +0000

    completed more edabits

--- 20200226.js
+++ 20200226.js
@@ -1,3 +1,48 @@
+// The Farm Problem
 function animals(chickens, cows, pigs) {
 	return (chickens*2)+(cows+pigs)*4;
 }
+
+// Name Greeting!
+const helloName = name => {
+	// return "Hello " + name + "!";
+    return `Hello ${name}!`
+}
+
+// Find the Bug: Returning the Container
+function getContainer(product) {
+	let container
+  switch (product) {
+		case "Bread":
+			container = "bag"
+			break
+		case "Beer":
+		case "Milk":
+			container = "bottle"
+			break
+		case "Cerials":
+			container = "box"
+			break
+		case "Eggs":
+			container = "carton"
+			break
+		case "Candy":
+			container = "plastic"
+			break
+		default:
+			container = null
+	}
+
+	return container
+}
+
+// Find the Index (Part 2)
+function search(arr, item) {
+	return arr.indexOf(item);
+}
+
+// ES6: Destructuring Arrays IV
+// const str = '[head, tail] = [1, 2, 3, 4]'
+const str = '[head, ...tail] = [1, 2, 3, 4]'
+
+
