ee479397752274b286ff9b18e4f962c915242ebe
NomadDaniel <reachingdaniel@gmail.com>
2020-02-28 00:01:36 +0000

    more edabits

--- 20200227.js
+++ 20200227.js
@@ -3,3 +3,17 @@ function isLastCharacterN(word) {
 	return word.endsWith('n');
 }
 
+// ES6: Destructuring Objects VIII
+const user = { name: "John", email: "john@example.com",city: "Phoenix", state: "AZ", country: "USA"}
+const str = `({ name, email, ...rest} = user ).toString()`
+
+// Stack the Boxes
+function stackBoxes(n) {
+  return n*n;
+}
+
+// Volume of a Box
+function volumeOfBox(sizes) {
+	return sizes.length * sizes.width * sizes.height;
+}
+
