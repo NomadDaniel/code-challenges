ae1bf6e50c134d4e38ea5a2e05f39c00f524c22e
NomadDaniel <reachingdaniel@gmail.com>
2020-02-29 18:53:35 +0000

    edabits, loop focus

---
+++ 20200229.js
@@ -0,0 +1,13 @@
+//Add the Index
+
+//***Given Code...
+//function addIndexes(arr) {
+    //}
+
+//Solution
+const addIndexes = arr => arr.map((v,i) => v + i);
+
+//Solution
+//function addIndexes(arr) {
+    //return arr.map((val, i) => val + i);
+    //}
