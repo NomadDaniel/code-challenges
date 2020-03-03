e5231064ce19470d4e7a98fbdf80d5441e1cdd1f
NomadDaniel <reachingdaniel@gmail.com>
2020-03-03 19:43:57 +0000

    finished more edabits, loops

--- 20200303.js
+++ 20200303.js
@@ -1,4 +1,4 @@
-// Convert Number to String of Dashes
+// ***Convert Number to String of Dashes***
 // Create a function that takes a number (from 1 - 60) and returns a corresponding string of hyphens.
 // // Initial Code...
 // function Go(num) {
@@ -21,3 +21,36 @@ const Go = num => '-'.repeat(num);
 //function Go(num) {
 	//return "-".repeat(num);
 //}
+
+// ____________________________________________________________
+
+// ***Word Endings***
+// Create a function that adds a string ending to each member in an array.
+// Solution
+function addEnding(arr, ending) {
+	return arr.map(x => x.concat(ending));
+}
+
+// Solution
+// function addEnding(arr, ending) {
+	// return arr.map(word => word + ending);
+// }
+
+// _____________________________________
+
+// Edabit
+// Write a function that takes an integer and returns a string with the given number of "a"s in Edabit.
+
+// Initial Code...
+//function howManyTimes(num) {
+	
+//}
+
+// Solution
+const howManyTimes = num => `Ed${'a'.repeat(num)}bit`
+
+// Solution
+// function howManyTimes(num) {
+//   let str = "a";
+//   return `Ed${str.repeat(num)}bit`;
+// }
