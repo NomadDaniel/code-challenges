37bc8164faf18721d5683f43c0f16b5d9d439aab
NomadDaniel <reachingdaniel@gmail.com>
2020-03-03 17:56:38 +0000

    edabits, loops

---
+++ 20200303.js
@@ -0,0 +1,23 @@
+// Convert Number to String of Dashes
+// Create a function that takes a number (from 1 - 60) and returns a corresponding string of hyphens.
+// // Initial Code...
+// function Go(num) {
+	
+// }
+
+//Solution
+const Go = num => '-'.repeat(num);
+
+//Solution
+//function Go(num) {
+//var str = '';
+	//for(var i = 0; i < num; i++) {
+	//str += '-';
+	//}
+	//return str;
+//}
+
+//Solution
+//function Go(num) {
+	//return "-".repeat(num);
+//}
