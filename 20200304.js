4638948a9e13887af31cb8fe9df1a9f708debd1a
NomadDaniel <reachingdaniel@gmail.com>
2020-03-05 02:57:01 +0000

    edabits, .map, array method, loops

---
+++ 20200304.js
@@ -0,0 +1,10 @@
+//Get Student Names
+//Create a function that takes an array of students and returns an array of student names.
+
+//Solution
+function getStudentNames(students) {
+	return students.map(x => x.name)
+}
+
+//Solution
+//const getStudentNames = students => students.map(student => student.name)
